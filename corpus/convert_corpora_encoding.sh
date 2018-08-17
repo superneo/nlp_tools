5b9a6f47abe1bdfca04c1215754351409e61a9b6
KeunYoung Park <superneo77@gmail.com>
2018-08-18 02:04:31 +0900

    feat: Enhance the script with bug fix/OS type check/iconv error check

--- corpus/convert_corpora_encoding.sh
+++ corpus/convert_corpora_encoding.sh
@@ -10,15 +10,25 @@ TGT_CORPORA_PATH=$2
 TGT_ENCODING=$3
 
 if [ -d "$TGT_CORPORA_PATH" ]; then
-  mkdir $TGT_CORPORA_PATH
+  rm -rf $TGT_CORPORA_PATH
 fi
 mkdir -p $TGT_CORPORA_PATH
 
+if [[ $OSTYPE == darwin* ]] ; then
+  file_flag=I
+elif [[ $OSTYPE == linux* ]] ; then
+  file_flag=i
+else
+  echo "[ERROR] This OS is not supported."
+  exit 1
+fi
+
 for src_corpus in `ls $SRC_CORPORA_PATH/*.txt`; do
-  echo "[processing] $src_corpus"
-  file_result=`file -I $src_corpus`
-  src_encoding=`echo $file_result | cut -d'=' -f2`
-  echo $src_encoding
+  src_encoding=`file -$file_flag $src_corpus | cut -d'=' -f2`
   iconv -f $src_encoding -t $TGT_ENCODING $src_corpus > ${TGT_CORPORA_PATH}/${src_corpus##*/}
+  if [ $? -ne 0 ] ; then
+    echo "[ERROR] iconv failed to convert $src_corpus of the $src_encoding encoding"
+    rm -rf ${TGT_CORPORA_PATH}/${src_corpus##*/}
+  fi
 done
 
