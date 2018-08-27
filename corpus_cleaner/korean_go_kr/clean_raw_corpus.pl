c8b4d8bb317ca19c5db69d48940ef3bc13adabd4
KeunYoung Park <superneo77@gmail.com>
2018-08-28 00:58:28 +0900

    feat: Enhance the regex for korean_go_kr corpora (in progress)

--- corpus_cleaner/korean_go_kr/clean_raw_corpus.pl
+++ corpus_cleaner/korean_go_kr/clean_raw_corpus.pl
@@ -29,6 +29,10 @@ if (-d $cleaned_corpora_dir) {
 die "[ERROR] failed to make cleaned corpus directory $cleaned_corpora_dir: $!\n"
     unless (mkdir $cleaned_corpora_dir);
 
+# characters/symbols to remove globally in each line (some may require to be escaped)
+my @del_chars = qw ( ○ ▴ );
+my $pattern = join("|", @del_chars);
+
 opendir(RAW_CORPORA_DIR, $raw_corpora_dir)
     or die "[ERROR] failed to open $raw_corpora_dir: $!\n";
 opendir(CLEANED_CORPORA_DIR, $cleaned_corpora_dir)
@@ -46,22 +50,37 @@ foreach my $corpus (@raw_corpora) {
 
     while (my $line = <$ifh>) {
         next if ($line !~ m/\p{InHangul_Syllables}/);
+        next if ($line =~ m/BR.*\.txt.*원본:.*\.hwp/);  # ex) BRBB0062.txt, 원본:bb93b002.hwp
         chomp $line;
         $line = decode_entities($line);
 
         # text cleaning
+        $line =~ s/^\(([^\(\)]+)\)$/$1/g;
         $line =~ s/::+//g;  # for cases like "그~ 김치후선생::이"
-        $line =~ s/(\p{InHangul_Syllables})~([\s\p{P}])/$1$2/g;  # for cases like "인민들과 만나지 않고 이~ 저::~,"
-        $line =~ s/(\p{L})_(\p{L})/$1$2/g;
+        $line =~ s/(\p{InHangul_Syllables})~([\s\p{P}])/$1$2/g;  # ex) "인민들과 만나지 않고 이~ 저::~,"
+        $line =~ s/\`/'/g;
+        $line =~ s/[「」]/"/g;
+        $line =~ s/[【】◀▶]//g;
+        $line =~ s/^" | " //g;  # caution
+        $line =~ s/ ; | - / /g;
+        $line =~ s/(\p{L})_(\p{L})/$1 $2/g;
+        # for cases of literary corpora like "▴ 오빠 = 별로 없다."
+        $line =~ s/^(\>\> |\*+ |◆ ?|▴ ?|◇ ?|\<\> +|● ?|── +|\|\| +|\! +)(\p{InHangul_Syllables}+\s*=\s*)?//;
+        $line =~ s/$pattern//g;
         $line =~ s/<[^<>]+$|^[^<>]+>//g;
         $line =~ s/<[^\"<>]*\"((?:[^\"\p{InHangul_Syllables}]*\p{InHangul_Syllables}+[^\"\p{InHangul_Syllables}]*)+)\"[^\"<>]*>/$1/g;
         $line =~ s/<((?:[^<\/>\p{InHangul_Syllables}]*\p{InHangul_Syllables}+[^<\/>\p{InHangul_Syllables}]*)+)>/$1/g;
+        $line =~ s/(<\/?(div|br|p|li|dt|dd).*?\/?>\s*)+/ /ig;
         $line =~ s/<[^<>]+>//g;
         $line =~ s/ / /g;  # nbsp
         $line =~ s/\x{FEFF}//g;  # zero width nbsp
         $line =~ s/ +/ /g;
         $line = trim $line;
-        $line =~ s/\,$//;  # for only colloquial corpora
+        $line =~ s/\,$//;  # for colloquial corpora
+        $line =~ s/^- | -$//g;  # for colloquial corpora
+        $line =~ s/\. +"$/\."/;
+        $line =~ s/(\d+)\. +(\d+)%/$1\.$2%/g;  # for cases of literary corpora like "49. 4%""
+        $line =~ s/""+//g;
         next if ($line =~ m/^\s*$/);
 
         print $ofh $line."\n";
@@ -73,3 +92,4 @@ foreach my $corpus (@raw_corpora) {
 
 closedir(CLEANED_CORPORA_DIR);
 closedir(RAW_CORPORA_DIR);
+
